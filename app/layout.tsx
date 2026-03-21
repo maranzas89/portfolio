7e46131fb987a71255a250ed2beeb49460d2b014
maranzas89 <layumiw@hotmail.com>
2026-03-20 21:44:37 -0700

    test deploy update

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,6 +60,15 @@ export default function RootLayout({
           src="https://t.contentsquare.net/uxa/6ad696d06b974.js"
           strategy="afterInteractive"
         />
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vz24r1ysdg");
+          `}
+        </Script>
         {children}
         <AskWenShell />
         <ConditionalAnalytics />
